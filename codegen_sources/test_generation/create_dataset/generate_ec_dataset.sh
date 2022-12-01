40268c5d1ec1c276aa02e6cbb787e37fea0ba2d6
minxue29031 <115342040+minxue29031@users.noreply.github.com>
2022-12-01 20:14:08 +0800

    Update generate_ec_dataset.sh

--- codegen_sources/test_generation/create_dataset/generate_ec_dataset.sh
+++ codegen_sources/test_generation/create_dataset/generate_ec_dataset.sh
@@ -1,11 +1,9 @@
-INPUT_DATA='data/parallel_corpus/extract/ori_java.sa.tok'
 MODEL_PATH='models/TransCoder_model_2.pth'
 BPE_PATH='data/bpe/cpp-java-python/codes'
 INPUT_DATASET='data/parallel_corpus/extract'
 OUTPUT_EC_PATH='data/parallel_corpus/error_correction_dataset'
 
 python -m codegen_sources.test_generation.error_correction_dataset\
-    --input_data $INPUT_DATA\
     --model_path $MODEL_PATH\
     --bpe_path $BPE_PATH\
     --input_path $INPUT_DATASET\
