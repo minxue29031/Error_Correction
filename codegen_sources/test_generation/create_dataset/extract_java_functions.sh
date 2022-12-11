f127e714f8c0adfe6dff2fa2fa4bb279a0f2942a
minxue29031 <115342040+minxue29031@users.noreply.github.com>
2022-12-11 09:43:03 +0800

    Add files via upload

---
+++ codegen_sources/test_generation/create_dataset/extract_java_functions.sh
@@ -0,0 +1,11 @@
+EXTRACT_DATASET='data/parallel_corpus/extract'
+INPUT_ALL='data/parallel_corpus/results/transcoder_outputs/python_transcoder_translation.csv'
+INPUT_JAVA_CONTENT='data/parallel_corpus/offline_dataset/train.java_sa-python_sa.java_sa.tok' 
+TRANSLATED_FUNC='data/parallel_corpus/offline_dataset/train.java_sa-python_sa.python_sa.tok' 
+
+
+python -m codegen_sources.test_generation.extract_java_BT\
+    --output_path $EXTRACT_DATASET\
+    --csv_path $INPUT_ALL\
+    --extract_target $INPUT_JAVA_CONTENT\
+    --translated_func $TRANSLATED_FUNC\
