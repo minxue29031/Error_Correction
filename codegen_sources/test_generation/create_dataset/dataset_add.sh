b3ccae5e19dc8c2b2de13a0198cf0a800afa5035
minxue29031 <115342040+minxue29031@users.noreply.github.com>
2022-12-12 09:45:22 +0800

    Add files via upload

---
+++ codegen_sources/test_generation/create_dataset/dataset_add.sh
@@ -0,0 +1,13 @@
+MODEL_PATH='models/TransCoder_model_2.pth'
+BPE_PATH='data/bpe/cpp-java-python/codes'
+INPUT_DATASET='data/test_dataset'
+OUTPUT_FLODER='data/parallel_corpus_test'
+OUTPUT_EC_PATH='data/parallel_corpus_test'
+
+python -m codegen_sources.test_generation.dataset_add\
+    --model_path $MODEL_PATH\
+    --bpe_path $BPE_PATH\
+    --pre_data_path $INPUT_DATASET\
+    --output_floder_path $OUTPUT_FLODER\
+    --output_path $OUTPUT_EC_PATH\
+
