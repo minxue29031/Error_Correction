8a89ae046ed779d411f281ed0b52a32ba22cbfd6
minxue29031 <115342040+minxue29031@users.noreply.github.com>
2023-06-12 15:23:22 +0200

    Update pre_extract_EOS_pair.py

--- codegen_sources/build_small_datastore/pre_extract_EOS_pair.py
+++ codegen_sources/build_small_datastore/pre_extract_EOS_pair.py
@@ -9,7 +9,6 @@ def process_search_files(input_value_file, input_inputs_file, output_file):
         pre_search_inputs_content = file.readlines()
 
     search_results = []
-
     for index, line in enumerate(pre_search_value_content):
         number = int(line.strip())
         if number == 1:
@@ -18,9 +17,9 @@ def process_search_files(input_value_file, input_inputs_file, output_file):
 
     with open(output_file, 'w') as file:
         file.writelines(search_results)
-
     print("Processing complete! Results have been saved to the file", output_file, ".")
 
+    
 def process_search_inputs(input_file, output_file):
     with open(input_file, 'r') as file:
         content = file.read()
@@ -31,9 +30,9 @@ def process_search_inputs(input_file, output_file):
 
     with open(output_file, 'w') as file:
         file.write(content)
-
     print("Processing complete! Results have been saved to the file", output_file, ".")
 
+    
 def extract_values(input_file, output_file):
     with open(input_file, 'r') as file:
         content = file.read()
@@ -43,15 +42,14 @@ def extract_values(input_file, output_file):
 
     with open(output_file, 'w') as file:
         file.write(result_string)
-
     print("Processing complete! Results have been saved to the file", output_file, ".")
 
+    
 def pre_extract_EOS_pairs(input_file, output_file):
     with open(input_file, 'r') as file:
         content = file.read()
 
     content = content.replace("</s>", "")
-
     pattern = r"\[\[array\(\[(.*?)\],"
     matches = re.findall(pattern, content, re.S)
 
@@ -63,18 +61,18 @@ def pre_extract_EOS_pairs(input_file, output_file):
             second_column = re.sub(r"^[\'\",\s]+|[\'\",\s]+$", '', parts[1])
             data.append([first_column, second_column])
 
-
-
     with open(output_file, "w", newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(["fail_input", "succ_prefix"])
         writer.writerows(data)
 
+        
 def main():
     extract_values('search_value.sa.tok', 'pre_search_value.sa.tok')
     process_search_inputs('search_inputs.sa.tok', 'pre_search_inputs.sa.tok')
     process_search_files('pre_search_value.sa.tok', 'pre_search_inputs.sa.tok', 'EOS_search_inputs.sa.tok')
     pre_extract_EOS_pairs('EOS_search_inputs.sa.tok', 'extract_EOS_pairs.csv')
 
+    
 if __name__ == "__main__":
     main()
